4cb9385e42f6b8076f29f6c42985ecd94474ab92
Jessica Zacharias <98563366+jzachari@users.noreply.github.com>
2023-06-30 11:21:38 -0700

    Add files via upload

--- blocks/shiftbackground/shiftbackground.js
+++ blocks/shiftbackground/shiftbackground.js
@@ -31,51 +31,55 @@ function rgbToHex(r, g, b) {
 // Wrapper function for the shiftColor function
 function shiftColorWrapper() {
   let id = null;
+  // Set the maxium value that r, b or g can shift by 50
+  const maxShift = 100;
+  // get the value for the css variable --highlight-background-color
+  const color = getComputedStyle(document.documentElement).getPropertyValue('--highlight-background-color');
+  // Convert the color to rgb
+  const rgb = hexToRgb(color);
+  // write rgb to the console
+  // console.log(rgb);
+  // Save the original rgb values
+  const originalr = rgb[0];
+  const originalg = rgb[1];
+  const originalb = rgb[2];
   // increment the r, g, and b values by 1 until they reach the maxShift value
   function shiftColor() {
-    // Set the maxium value that r, b or g can shift by 50
-    const maxShift = 50;
-    // get the value for the css variable --highlight-background-color
-    const color = getComputedStyle(document.documentElement).getPropertyValue('--highlight-background-color');
-    // Convert the color to rgb
-    const rgb = hexToRgb(color);
-    // write rgb to the console
-    // console.log(rgb);
-    // Save the original rgb values
-    const original = rgb;
     // Increment the r, g, and b values by 1
     rgb[0] += 1;
     rgb[1] += 1;
     rgb[2] += 1;
+    // write rgb[0], rgb[1], and rgb[2] to the console
+    // console.log(rgb[0], rgb[1], rgb[2]);
     // if r, g, or b is equal to or greater than 255, set it to 255
     if (rgb[0] >= 255) {
       rgb[0] = 255;
-      // clear the interval id
-      clearInterval(id);
     }
     if (rgb[1] >= 255) {
       rgb[1] = 255;
-      // clear the interval id
-      clearInterval(id);
     }
     if (rgb[2] >= 255) {
       rgb[2] = 255;
-      // clear the interval id
+    }
+    // If rgb[0] and rgb [1] and rgb[2] are all equal to 255, clear the interval id
+    if (rgb[0] === 255 && rgb[1] === 255 && rgb[2] === 255) {
       clearInterval(id);
     }
     // if r, g, or b is equal to or greater than original + maxShift, set it to original + maxShift
-    if (rgb[0] >= original[0] + maxShift) {
-      rgb[0] = original[0] + maxShift;
+    if (rgb[0] >= Number(originalr) + Number(maxShift)) {
+      // Write "maxshift reached for rgb0" to the console
+      // console.log('maxshift reached for rgb0');
+      rgb[0] = originalr + maxShift;
       // clear the interval id
       clearInterval(id);
     }
-    if (rgb[1] >= original[1] + maxShift) {
-      rgb[1] = original[1] + maxShift;
+    if (rgb[1] >= Number(originalg) + Number(maxShift)) {
+      rgb[1] = Number(originalg) + Number(maxShift);
       // clear the interval id
       clearInterval(id);
     }
-    if (rgb[2] >= original[2] + maxShift) {
-      rgb[2] = original[2] + maxShift;
+    if (rgb[2] >= Number(originalb) + Number(maxShift)) {
+      rgb[2] = Number(originalb) + Number(maxShift);
       // clear the interval id
       clearInterval(id);
     }
@@ -87,6 +91,6 @@ function shiftColorWrapper() {
     document.documentElement.style.setProperty('--highlight-background-color', newhex);
   }
   // Call the shiftColor function every 500 milliseconds and set it to the const id
-  id = setInterval(shiftColor, 500);
+  id = setInterval(shiftColor, 200);
 }
 shiftColorWrapper();
