c8be86d8404624bd0bd83b7e5580b30e1371808b
Jessica Zacharias <98563366+jzachari@users.noreply.github.com>
2023-12-12 15:42:31 -0800

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,23 @@ import { sampleRUM } from './lib-franklin.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+function jQueryIsReady() {
+  // eslint-disable-next-line no-undef
+  $('.hero-wrapper').ripples({
+    resolution: 1024,
+    dropRadius: 40,
+    perturbance: 0.09,
+  });
+}
+
+function checkJquery() {
+  if (typeof jQuery === 'undefined') {
+    return false;
+  }
+  // eslint-disable-next-line no-use-before-define
+  clearTimeout(interval);
+  jQueryIsReady();
+  return true;
+}
+const interval = setInterval(checkJquery, 1000);
